ff7ca042bd621964aaddb5e67eb85cae79d47f92
kesavaprasad08 <kesav2661998@gmail.com>
2023-06-21 17:40:01 +0530

    Subscribing and Dispatching Actions

--- src/store/index.js
+++ src/store/index.js
@@ -21,6 +21,16 @@ const counterReducer = (state = {counter:0},action) => {
         counter:state.counter-2,
     };
   }
+  if(action.type === 'INCREMENTBY5'){
+    return {
+        counter:state.counter+5,
+    };
+  }
+  if(action.type === 'DECREMENTBY5'){
+    return {
+        counter:state.counter-5,
+    };
+  }
   
   return state;
 };
