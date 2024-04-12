bc2ce515c7e788c9121dbe83be4e26184cbc3e29
kesavaprasad08 <kesav2661998@gmail.com>
2024-04-12 21:36:51 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import { Fragment } from 'react';
-import { useSelector } from 'react-redux';
+import { Fragment } from "react";
+import { useSelector } from "react-redux";
 
-import Counter from './components/Counter';
-import Header from './components/Header'
-import Auth from './components/Auth'
-import UserProfile from './components/UserProfile'
+import Counter from "./components/Counter";
+import Header from "./components/Header";
+import Auth from "./components/Auth";
+import UserProfile from "./components/UserProfile";
 
 function App() {
-const isAuth =  useSelector(state => state.auth.isAuthenticated)
+  const isAuth = useSelector((state) => state.auth.isAuthenticated);
   return (
     <Fragment>
       <Header />
-      {!isAuth &&<Auth />}
-      {isAuth && <UserProfile/> }
-    <Counter />
+      {!isAuth && <Auth />}
+      {isAuth && <UserProfile />}
+      <Counter />
     </Fragment>
   );
 }
